257758232001a95e0caab0473c446d66ad2be581
Pandaaa-C <pandacodeyt@gmail.com>
2026-09-16 12:21:22 +0100

    edit: vehicle entity

--- src/client/entities/vehicle.ts
+++ src/client/entities/vehicle.ts
@@ -1,19 +1,52 @@
-import { Entity } from "./entity";
+import {Entity} from "./entity";
 
 export class Vehicle extends Entity {
-	get speed(): number {
-		return GetEntitySpeed(this.handle);
-	}
+    get speed(): number {
+        return GetEntitySpeed(this.handle);
+    }
 
-	get engineOn(): boolean {
-		return GetIsVehicleEngineRunning(this.handle);
-	}
+    get bodyHealth(): number {
+        return GetVehicleBodyHealth(this.handle);
+    }
 
-	set engineOn(v: boolean) {
-		SetVehicleEngineOn(this.handle, v, true, true);
-	}
+    get dashboardColor(): number {
+        return GetVehicleDashboardColor(this.handle);
+    }
 
-	repair(): void {
-		SetVehicleFixed(this.handle);
-	}
+    get dead(): boolean {
+        return IsEntityDead(this.handle);
+    }
+
+    get engine(): boolean {
+        return GetIsVehicleEngineRunning(this.handle);
+    }
+
+    get engineHealth(): number {
+        return GetVehicleEngineHealth(this.handle);
+    }
+
+    get highBeams(): boolean {
+        const highBeamValue = Number(GetVehicleLightsState(this.handle)[2]);
+        return highBeamValue === 1;
+    }
+
+    get engineOn(): boolean {
+        return GetIsVehicleEngineRunning(this.handle);
+    }
+
+    set engineOn(v: boolean) {
+        SetVehicleEngineOn(this.handle, v, true, true);
+    }
+
+    repair(): void {
+        SetVehicleFixed(this.handle);
+    }
+
+    destroy() {
+        DeleteVehicle(this.handle);
+    }
+
+    explode() {
+        ExplodeVehicle(this.handle, true, true);
+    }
 }
--- src/server/managers/vehicle-pool.ts
+++ src/server/managers/vehicle-pool.ts
@@ -1,16 +1,30 @@
-import { Vehicle } from "../entities/vehicle";
-import { ServerPool } from "./server-pool";
+import {Vehicle} from "../entities/vehicle";
+import {ServerPool} from "./server-pool";
+import {IVector3} from "../../shared";
 
 export class VehiclePool extends ServerPool<Vehicle> {
-	protected ids(): number[] {
-		return GetAllVehicles() as number[];
-	}
+    protected ids(): number[] {
+        return GetAllVehicles() as number[];
+    }
 
-	protected wrap(id: number): Vehicle {
-		return new Vehicle(id);
-	}
+    protected wrap(id: number): Vehicle {
+        return new Vehicle(id);
+    }
 
-	at(handle: number) {
-		return new Vehicle(handle);
-	}
+    at(handle: number) {
+        return new Vehicle(handle);
+    }
+
+    new(
+        hash: number | string,
+        position: IVector3,
+        heading: number,
+        isNetwork: boolean = true,
+        netMissionEntity: boolean = true,
+    ): Vehicle | null {
+        const _hash = typeof hash === "string" ? GetHashKey(hash) : hash;
+        const handle = CreateVehicle(_hash, position.x, position.y, position.z, heading, isNetwork, netMissionEntity);
+		
+        return handle !== 0 ? new Vehicle(handle) : null;
+    }
 }
